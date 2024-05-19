9214f4a26fca8a8b43cfdb5672de1a4b735517f7
xuewenya <s10262410@connect.np.edu.sg>
2024-05-19 14:15:43 +0800

    update challenge

--- app/src/main/java/sg/edu/np/mad/madpractical3/MessageGroup.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MessageGroup.java
@@ -1,12 +1,16 @@
 package sg.edu.np.mad.madpractical3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MessageGroup extends AppCompatActivity {
 
@@ -15,10 +19,29 @@ public class MessageGroup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_message_group);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnGroup1 = findViewById(R.id.btnGroup1);
+        btnGroup1.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View view) {
+               FragmentManager fragmentManager = getSupportFragmentManager();
+               fragmentManager.beginTransaction()
+                       .replace(R.id.fragmentContainerView2, Group1Fragment.class, null)
+                       .setReorderingAllowed(true)
+                       .addToBackStack("name")
+                       .commit();
+           }
+        });
+
+        Button btnGroup2 = findViewById(R.id.btnGroup2);
+        btnGroup2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, Group2Fragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
         });
     }
 }
